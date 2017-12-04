64bd22acf0e59c23201216a0a586b41856c28514
Dinesh Chouhan <dcmayankavi@gmail.com>
2017-12-05 00:20:55 +0530

    Select2 used to get products

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import jquery from 'jquery';
+import Select2 from './components/Select2/Select2';
+import './components/Select2/Select2.css';
 import './index.css';
+import Products from './products.json';
 
 const ProductList = ( props ) => {
 	return(
 		<tr>
 			<td>{props.index + 1}</td>
-			<td>{props.product.name}</td>
+			<td>{props.product.text}</td>
 			<td>&#x20b9; {props.product.price}</td>
 			<td>{props.product.quantity}</td>
 			<td>&#x20b9; {props.product.price * props.product.quantity}</td>
@@ -18,29 +22,101 @@ const ProductList = ( props ) => {
 	)
 }
 
-const ProductForm = ( props ) => {
-	return(
-		<form onSubmit={ (event) => {
-			event.preventDefault();
-			document.getElementsByName('name')[0].focus();
-			if ( props.product.name && 0 != props.product.price && 0 != props.product.quantity ) {
-				props.addProduct(event);
-			}
-		}} >
-			<table className="add-record">
-				<tbody>
-					<tr>
-						<td width="*"><input type="text" placeholder="Name" name="name" value={props.product.name} onChange={props.updateProduct} /></td>
-						<td width="15%"><input type="number" placeholder="Price" name="price" value={props.product.price} onChange={props.updateProduct} /></td>
-						<td width="15%"><input type="number" placeholder="Quantity" name="quantity" value={props.product.quantity} onChange={props.updateProduct} /></td>
-						<td width="25%"><button type="submit">Add Product</button></td>
-					</tr>
-				</tbody>
-			</table>
-		</form>
-	)
-}
+class ProductForm extends React.Component {
 
+	constructor( props ){
+		super( props );
+		this.fetchProduct = this.fetchProduct.bind(this);
+		this.updateProduct = this.updateProduct.bind(this);
+		this.resetProduct = this.resetProduct.bind(this);
+		
+		var ProductsLength = Object.keys(Products).length,
+			products = [];
+		for ( var i = 1; i <= ProductsLength; i++ ) {
+			products.push( Products['product-'+i] );
+		}
+
+		this.state = {
+			products: products,
+			product: {
+				id: '',
+				text: '',
+				price: '',
+				quantity: ''
+			}
+		}
+	}
+
+	resetProduct() {
+		this.setState({
+			product: {
+				id: '',
+				text: '',
+				price: '',
+				quantity: ''
+			}
+		});
+	}
+
+	fetchProduct( event ) {
+		var index = event.target.value;
+		var new_product = Products['product-'+index];
+		this.setState({
+			product : {
+				id : new_product.id,
+				text : new_product.text,
+				price : new_product.price,
+				quantity : 0
+			}
+		});
+	}
+
+	updateProduct( event ){
+		var quantity = event.target.value;
+		var product = this.state.product;
+
+		product.quantity = quantity;
+		this.setState({
+			product
+		});
+	}
+
+	render(){
+		return(
+			<form onSubmit={ (event) => {
+				event.preventDefault();
+				document.getElementById('select2-name').focus();
+				if( this.state.product.id != 0 && this.state.product.quantity > 0 ) {
+					this.props.addProduct( this.state.product );
+					this.resetProduct();
+				}
+			}} >
+				<table className="add-record">
+					<tbody>
+						<tr>
+							<td width="*">
+								<Select2
+									data={this.state.products}
+									onSelect={this.fetchProduct}
+									id="select2-name"
+									value={this.state.product.id}
+									options={
+										{
+											placeholder: 'Search product...',
+										}
+									}
+								/>
+							</td>
+							<td width="15%"><label className="product-price">&#x20b9; {this.state.product.price}</label></td>
+							<td width="15%"><input type="number" placeholder="Quantity" name="quantity" value={this.state.product.quantity} onChange={this.updateProduct} /></td>
+							<td width="25%"><button type="submit">Add Product</button></td>
+						</tr>
+					</tbody>
+				</table>
+			</form>
+		)
+	}
+}
 const TotalAmount = ( props ) => {
 	var products = props.products;
 	var total = 0;
@@ -60,17 +136,10 @@ class MainWrapper extends React.Component {
 	
 	constructor(){
 		super();
-		this.updateProduct = this.updateProduct.bind(this);
 		this.addProduct = this.addProduct.bind(this);
 		this.removeProduct = this.removeProduct.bind(this);
 		this.state = {
-			products: [],
-			newProduct: {
-				id: 1,
-				name: '',
-				price: '',
-				quantity: ''
-			}
+			products: []
 		}
 	}
 
@@ -83,31 +152,11 @@ class MainWrapper extends React.Component {
 		});
 	}
 
-	updateProduct( product ) {
-		var newProduct = this.state.newProduct,
-			name = product.target.name;
-
-		newProduct[name] = product.target.value;
-		
+	addProduct( product ) {
+		var products = this.state.products;
+		products.push(product);
 		this.setState({
-			newProduct
-		})
-	}
-
-	addProduct() {
-		var products = this.state.products,
-			new_id = this.state.newProduct.id + 1;
-
-		products.push(this.state.newProduct);
-
-		this.setState({
-			products,
-			newProduct: {
-				id: new_id,
-				name: '',
-				price: '',
-				quantity: ''
-			}
+			products
 		})
 	}
 
@@ -124,8 +173,6 @@ class MainWrapper extends React.Component {
 		return (
 			<div>
 				<ProductForm 
-					product={this.state.newProduct}
-					updateProduct={this.updateProduct}
 					addProduct={this.addProduct} 
 				/>
 
